f0dcd77c95ba8d6742138d6fb216698e4ab37628
Lipin Vlad <144615805+DoAsPlanned@users.noreply.github.com>
2025-12-18 13:02:10 +0300

    3

---
+++ laba3/Temp/Includes/Globalvar.h
@@ -0,0 +1,40 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1764938919_2_
+#define _BUR_1764938919_2_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_GLOBAL plcbit iNegHwEnd;
+_GLOBAL plcbit iPosHwEnd;
+_GLOBAL struct FB_Regulator fb_regulator;
+_GLOBAL struct SdcHwCfg_typ AxisX_HW;
+_GLOBAL struct SdcDiDoIf_typ AxisX_DiDoIf;
+_GLOBAL struct SdcEncIf16_typ AxisX_EncIf;
+_GLOBAL struct SdcDrvIf16_typ AxisX_DrvIf;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/Library/Library.fun\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1764938919_2_ */
+
